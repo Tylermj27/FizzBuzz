f612bc17f7abf3f041de06747b8671facf142969
Tylermj27 <47044753+Tylermj27@users.noreply.github.com>
2019-01-25 18:12:16 -0600

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,15 @@
+def main():
+    #Going through a for loop to print everything from 1-100
+    for x in range(1, 101):
+        if x % 3 == 0 and not x % 5 == 0:
+            print("Fizz")
 
+        elif x % 5 == 0 and not x % 3 == 0:
+            print("Buzz")
+
+        elif x % 3 == 0 & x % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(x)
+  
+main()
